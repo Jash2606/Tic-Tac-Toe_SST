59f4c829df00e190d7c21dab061e2520e7e4f318
Jash Savaliya <jash.23bcs10163@sst.scaler.com>
2024-04-23 23:23:01 +0530

    Created All  Models ...

---
+++ Models/GameState.java
@@ -0,0 +1,5 @@
+package Models;
+import java.util.*;
+public enum GameState {
+    IN_PROGRESS, FINISHED, DRAW;
+}
---
+++ Models/Player.java
@@ -0,0 +1,6 @@
+package Models;
+public class Player {
+    String name;
+    Symbol symbol;
+    PlayerType PlayerType;
+}
